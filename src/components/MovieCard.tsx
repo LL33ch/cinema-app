1b232eb5c3f39ccd8068d6aece60e4ec3571b5a9
MrRobot <s.garazh2002@gmail.com>
2023-12-10 15:35:56 +0300

    v1.2.1 Fixes Img LCP

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -28,7 +28,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
 				<div className='dark:bg-zinc-900/50 bg-white dark:bg-gradient-to-r from-black via-black/70 backdrop-brightness-50 backdrop-blur-sm hover:backdrop-blur-none hover:border-zinc-600 p-5 border rounded-lg transition duration-150 ease-in-out hover:shadow-lg'>
 					<Link href={`/movie/${movie.kinopoiskId}`} passHref>
 						<div className='grid grid-cols-[auto_1fr_auto] gap-5'>
-							<Image src={movie.posterUrl} width={72} height={108} alt={movie.nameRu} priority />
+							<Image className='w-[80] h-[116]' src={movie.posterUrl} width={80} height={116} alt={movie.nameRu} priority />
 							<div>
 								<h2 className='font-bold text-xl'>{movie.nameRu}</h2>
 								<h3 className='text-sm mt-2'>{movie.nameOriginal ? `${movie.nameOriginal},` : ''} {movie.year}</h3>
