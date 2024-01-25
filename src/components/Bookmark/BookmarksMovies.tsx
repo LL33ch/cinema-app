7b21dc9fbe41eefd7204a00c190be7396b9f3996
MrRobot <s.garazh2002@gmail.com>
2024-01-25 21:58:13 +0300

    v1.4.7 fixed

--- src/components/Bookmark/BookmarksMovies.tsx
+++ src/components/Bookmark/BookmarksMovies.tsx
@@ -2,18 +2,20 @@
 import MovieList from '../MoviesList/MoviesList';
 
 export default function BookmarksMovies() {
-	const currentBookmarks = localStorage.getItem('bookmarks');
-	if (!currentBookmarks) {
+	if (typeof window !== 'undefined') {
+		const currentBookmarks = localStorage.getItem('bookmarks');
+		if (!currentBookmarks) {
+			return (
+				<div className='container px-2'>
+					У вас нет закладок.
+				</div>
+			)
+		}
+		const moviesData = JSON.parse(currentBookmarks)
 		return (
 			<div className='container px-2'>
-				У вас нет закладок.
+				<MovieList movies={moviesData} />
 			</div>
 		)
 	}
-	const moviesData = JSON.parse(currentBookmarks)
-	return (
-		<div className='container px-2'>
-			<MovieList movies={moviesData} />
-		</div>
-	)
 }
